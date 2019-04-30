94bceed7c9c0b22fe19d969c15db3aecc4fcaaf8
hanneslindstrom <halindst@gmail.com>
2019-04-30 01:14:25 -0700

    Wrong output

--- preprocessing/preprocess_digraphs.py
+++ preprocessing/preprocess_digraphs.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import hashlib
+from tqdm import tqdm
 
 KEYS = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "Space", "LShiftKey", "RShiftKey", "Back", "Oemcomma", "OemPeriod", "NumPad0", "NumPad1", "NumPad2", "NumPad3", "NumPad4", "NumPad5", "NumPad6", "NumPad7", "NumPad8", "NumPad9", "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9"]
 
@@ -29,7 +30,6 @@ def parse_raw_data(read_path, write_path, session_fraction=1, special_keys=True)
 			print file_name
 
 			openDigraphs = {} # first_key||second_key : [first_PT, first_RT, second_PT, second_RT]
-			openDigraphLookup = [[],[]] # [[key, ...], [[first_key||second_key, first_or_second=0/1], ...]]
 			lastPressedKey = None # (key, PT, RT)
 
 			for i, line in enumerate(file):
@@ -43,49 +43,42 @@ def parse_raw_data(read_path, write_path, session_fraction=1, special_keys=True)
 					continue
 
 				if action == "KeyDown":
-					try: #fail if first digraph
+					if lastPressedKey != None:
 						digraph = lastPressedKey[0] + "+" + key
 						openDigraphs[digraph] = [lastPressedKey[1], lastPressedKey[2], time, None]
-						openDigraphLookup[0].append(lastPressedKey[0])
-						openDigraphLookup[1].append((digraph, 0))
-						openDigraphLookup[0].append(key)
-						openDigraphLookup[1].append((digraph, 1)) 
-					except:
-						pass
-					lastPressedKey = (key, time, None)
+					lastPressedKey = [key, time, None]
 
 				elif action == "KeyUp":
-					for index in [i for i, x in enumerate(openDigraphLookup[0]) if x == key]:
-						digraph = openDigraphLookup[1][index][0]
-						firstOrSecond = openDigraphLookup[1][index][1]
-						print digraph
-						openDigraphs[digraph][1+2*firstOrSecond] = time
 
-						if None not in openDigraphs[digraph]:
+					if bool(openDigraphs) == False and lastPressedKey != None:
+						lastPressedKey[2] = time
+
+					deleteSet = []
+					for key_iterator in openDigraphs:
+
+						if key not in key_iterator.split("+"):
+							continue
+
+						#update digraph containing key
+						firstOrSecond = int(key_iterator.split("+")[1] == key)
+						openDigraphs[key_iterator][1+2*firstOrSecond] = time
+
+						#if digraph finished
+						if None not in openDigraphs[key_iterator]:
+
 							# Append to output
-							ht1 = int(openDigraphs[digraph][1]) - int(openDigraphs[digraph][0])
-							ht2 = int(openDigraphs[digraph][3]) - int(openDigraphs[digraph][2])
-							ptp = int(openDigraphs[digraph][2]) - int(openDigraphs[digraph][0])
-							rtp = int(openDigraphs[digraph][2]) - int(openDigraphs[digraph][1])
-							key1 = digraph.split("+")[0]
-							key2 = digraph.split("+")[1]
+							ht1 = int(openDigraphs[key_iterator][1]) - int(openDigraphs[key_iterator][0])
+							ht2 = int(openDigraphs[key_iterator][3]) - int(openDigraphs[key_iterator][2])
+							ptp = int(openDigraphs[key_iterator][2]) - int(openDigraphs[key_iterator][0])
+							rtp = int(openDigraphs[key_iterator][2]) - int(openDigraphs[key_iterator][1])
+							key1 = key_iterator.split("+")[0]
+							key2 = key_iterator.split("+")[1]
 							output.append((key1, key2, ht1, ht2, ptp, rtp))
-							#delete digraph from openDigraphs
-							del openDigraphs[digraph]
-							#delete key->digraph from lookup
-							indecisToDelete = []
-							for index2 in [i for i, x in enumerate(openDigraphLookup[0]) if x == key1 or x == key2]:
-								if openDigraphLookup[1][index2][0] == digraph:
-									indecisToDelete.append(index2)
 
-					try:
-						print indecisToDelete
-						for index in indecisToDelete[::-1]:
-							del openDigraphLookup[0][index]
-							del openDigraphLookup[1][index]
-					except:
-						pass
-					
+							#delete digraph from openDigraphs
+							deleteSet.append(key_iterator)
+					for digraph in deleteSet:
+						del openDigraphs[digraph]
 
 			# Write processed data to file
 			try:
@@ -94,7 +87,7 @@ def parse_raw_data(read_path, write_path, session_fraction=1, special_keys=True)
 				pass
 			with open(write_file, "w+") as file:
 				for entry in output:
-					file.write(entry[0] + " " + str(entry[1]) + "\n")
+					file.write(entry[0] + " " + entry[1] + " " + str(entry[2]) + " " + str(entry[3]) + " " + str(entry[4]) + " " + str(entry[5]) + "\n")
 
 
 def main():
