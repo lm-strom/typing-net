80ce4d0ddee58fe14fc6a17a2d07f8056222dfd8
hanneslindstrom <halindst@gmail.com>
2019-05-01 11:25:48 -0700

    file path update

--- models/classifier_keras.py
+++ models/classifier_keras.py
@@ -8,7 +8,7 @@ from keras.layers import Dense, Flatten
 from keras.layers import Conv1D, MaxPooling1D
 from keras.layers import GRU
 
-DATA_PATH = "/Users/Hannes/Documents/typing-net/data/processed_data/"
+DATA_PATH = "/Users/Hannes/Downloads/typing-net/data/10_users/"
 
 # Constants
 FEATURE_LENGTH = 6
